359e796f956f71bc4031c501f864e5b8c27fc42e
Josh Sample <joshuawsample@gmail.com>
2018-10-04 09:16:40 -0400

    Replaced redux promise with custom middleware

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import reduxPromise from 'redux-promise';
+// import reduxPromise from 'redux-promise';
+import async from 'middlewares/async'
+//Custom 'async' middleware handling the work of ReduxPromise
 
 export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async)
         //reduxPromise - looks at action from creator and delays all requests
     )
     return (
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,10 +1,26 @@
 export default ({dispatch}) => next => action => {
+    //Check to see if the action has a promise on it's payload property
 
+    //If it does, then wait for resolution
+
+    //If it does not, then send the action on to the next middleware
+    
+    if(!action.payload || !action.payload.then) {
+        //not necessarily returning anything - just exiting function with next()
+        return next(action);
+    }
+
+    //We want to wait for promise to resolve (get data!) and then create a new action with the data and dispatch that data
+    action.payload.then(function(response){
+        const newAction = { ...action, payload: response };
+        //flows through middleware again without Promise from dispatch
+        dispatch(newAction)
+    })
 }
 
+//////////////ABOVE IS PRETTY MUCH THE SAME AS REDUXPROMISE
 
-
-//Same as above - three function change return...
+//Same as above - three function change return...particular to redux
 // export default function({dispatch}) {
 //     return function(next) {
 //         return function(action) {
