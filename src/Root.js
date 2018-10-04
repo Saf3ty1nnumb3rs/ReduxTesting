5d63753eff2cc937b8bc96eea5af3ce574105fc8
Josh Sample <joshuawsample@gmail.com>
2018-10-04 13:27:08 -0400

    Middleware state validator added - comment exercise complete

--- src/Root.js
+++ src/Root.js
@@ -4,18 +4,19 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
 // import reduxPromise from 'redux-promise';
 import async from 'middlewares/async'
+import stateValidator from 'middlewares/stateValidator';
 //Custom 'async' middleware handling the work of ReduxPromise
 
 export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(async)
+        applyMiddleware(async, stateValidator)
         //reduxPromise - looks at action from creator and delays all requests
-    )
+    ) 
     return (
         <Provider store={store}>
             {children}
         </Provider>
     )
-}
+}  
